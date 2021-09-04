95123783e23c3618fb86343f65cabdbe59473bfb
Erik <78443570+thekhachik@users.noreply.github.com>
2021-09-04 14:47:46 +0300

    Create p13.java

---
+++ p13.java
@@ -0,0 +1,16 @@
+import java.lang.String;
+
+public class Ball {
+
+    private int quantity;
+
+    public Ball(){
+
+        quantity = 16;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+}
