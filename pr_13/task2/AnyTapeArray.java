9d6326de5a4fe4d02a6ce63a3a617a03ca106f56
Erik <78443570+thekhachik@users.noreply.github.com>
2021-11-30 19:31:32 +0300

    Create AnyTapeArray.java

---
+++ pr_13/task2/AnyTapeArray.java
@@ -0,0 +1,20 @@
+package AnyTapeArray;
+
+public class AnyTapeArray <E> {
+	private E [] arr;
+	
+	public E getArrIndex(int i){
+		return  arr[i] ;
+	}
+	
+	public void setArr( E [] arr){
+		this.arr =  arr;
+	}
+	
+	public int getLength(){
+		return  arr.length ;
+	}
+	
+	
+
+}
